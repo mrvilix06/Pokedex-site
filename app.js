cea9a1decedddb853ec6db006fa35c6e9380fe3c
mrvilix06 <smaurin18@hotmail.fr>
2023-09-11 11:06:21 +0200

    Base du site

---
+++ app.js
@@ -0,0 +1,110 @@
+let allPokemon = [];
+let tableauFin = [];
+const searchInput = document.querySelector('.recherche-poke input');
+const listePoke = document.querySelector('.liste-poke');
+
+const types = {
+    grass: '#78c850',
+    ground: '#E2BF65',
+    dragon: '#6F35FC',
+    fire: '#F58271',
+    electric: '#F7D02C',
+    fairy: '#D685AD',
+    poison: '#966DA3',
+    bug: '#B3F594',
+    water: '#6390F0',
+    normal: '#D9D5D8',
+    psychic: '#F95587',
+    flying: '#A98FF3',
+    fighting: '#C25956',
+    rock: '#B6A136',
+    ghost: '#735797',
+    ice: '#96D9D6'
+};
+
+fetchPokemonBase();
+
+window.addEventListener('scroll', () => {
+    const {scrollTop, scrollHeight, clientHeight} = document.documentElement;
+
+    if(clientHeight + scrollTop >= scrollHeight - 20) {
+        addPoke(6);
+    }
+})
+
+let index = 21;
+function addPoke(nb) {
+    if(index > 151) {
+        return;
+    }
+    const arrToAdd = allPokemon.slice(index, index + nb);
+    createCard(arrToAdd);
+    index += nb;
+}
+
+function fetchPokemonBase() {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+        .then(reponse => reponse.json())
+        .then((allPoke) => {
+            allPoke.results.forEach((pokemon) => {
+                fetchPokemonComplet(pokemon);
+            })
+        })
+}
+
+function fetchPokemonComplet(pokemon) {
+    let objPokemonFull = {};
+    let url = pokemon.url;
+    let nameP = pokemon.name;
+
+    fetch(url)
+        .then(reponse => reponse.json())
+        .then((pokeData) => {
+            objPokemonFull.pic = pokeData.sprites.front_default;
+            objPokemonFull.type = pokeData.types[0].type.name;
+            objPokemonFull.id = pokeData.id;
+
+            fetch(`https://pokeapi.co/api/v2/pokemon-species/${nameP}`)
+                .then(reponse => reponse.json())
+                .then((pokeData) => {
+                    objPokemonFull.name = pokeData.names[4].name;
+                    allPokemon.push(objPokemonFull);
+
+                    if (allPokemon.length === 151) {
+                        tableauFin = allPokemon.sort((a, b) => {
+                            return a.id - b.id;
+                        }).slice(0, 21);
+                        createCard(tableauFin);
+                    }
+                })
+        })
+
+}
+
+function createCard(pokemon) {
+    for(let i = 0; i < pokemon.length; i++) {
+        const carte = document.createElement('li');
+        let couleur = types[pokemon[i].type];
+        carte.style.background = couleur;
+        const txtCarte = document.createElement('h5');
+        txtCarte.innerText = pokemon[i].name;
+        const idCarte = document.createElement('p');
+        idCarte.innerText = `ID# ${pokemon[i].id}`;
+        const imgCarte = document.createElement('img');
+        imgCarte.src = pokemon[i].pic;
+
+        carte.appendChild(imgCarte);
+        carte.appendChild(txtCarte);
+        carte.appendChild(idCarte);
+        listePoke.appendChild(carte);
+    }
+}
+
+//animation du bouton
+searchInput.addEventListener('input', function (e) {
+    if (e.target.value !== "") {
+        e.target.parentNode.classList.add('active-input');
+    } else if (e.target.value === "") {
+        e.target.parentNode.classList.remove('active-input');
+    }
+})
